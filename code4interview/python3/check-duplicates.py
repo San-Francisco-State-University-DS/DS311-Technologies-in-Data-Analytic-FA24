068f1249244211deb632c0771ef66a9a7710383e
Junjie@MacStudio.local <junjie@junjielei.com>
2022-09-01 16:48:18 -0700

    python check duplicates

---
+++ code4interview/python3/check-duplicates.py
@@ -0,0 +1,22 @@
+"""
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+Sample input and output:
+
+    Input: nums = [1,2,3,1]
+    Output: true
+
+    Input: nums = [1,2,3,4]
+    Output: false
+
+    Input: nums = [1,1,1,3,3,4,3,2,4,2]
+    Output: true
+"""
+
+
+def containsDuplicate(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: bool
+    """
