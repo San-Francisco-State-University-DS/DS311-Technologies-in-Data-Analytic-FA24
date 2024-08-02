767ff38bb2491ff81bad5d3e6f0788977e51517d
Norman Lo <lokman@mail.sfsu.edu>
2024-08-02 15:36:15 -0700

    update curriculum for SQL, Python, and R

---
+++ Week_02_SQL_Queries/Lab_Assignment/README.md
@@ -0,0 +1,15 @@
+## Instruction for the assignment
+
+Step 1: Open the Lab Exercise / Lab Assignment PDF file for the assignment instructions
+
+Step 2: Use one of the SQLite UI application (such as sqlite studio) to open the database file (.db) in the data folder.
+
+Step 3: Answer the question in the assignment file by querying the data from the database file. 
+
+Step 4: Copy and paste the query table to an excel file (one question per tab) and save it as .xlsx file format.
+
+Step 5: Save each query as a .txt file
+
+Step 6: Upload the excel file and text files to the assignment submission repo, then push to GitHub.
+
+Step 7: Submit the link to the submission repo on GitHub to Cavnas for grading. 
---
+++ Week_03_Python_Data_Structure/Python_CoinFlip.py
@@ -0,0 +1,58 @@
+import random
+
+
+def coin():
+    """simulate a coin flip()
+    Returns:
+        str: ["heads" or "tails"]
+    """
+
+
+def coin_10flips_1():
+    """Simulate 10 coin flips
+    Returns:
+        [list or string]: [lst should contains heads or tails]
+    """
+
+
+def coin_10flips_2():
+    """[The second way to flip a coin for 10 times]
+    """
+
+
+def count_head(n):
+    """count how many times the coin is HEAD and how many times the coin is TAIL
+    Args:
+        n (int): Number of times the coin is going to flipped
+    Returns:
+        Tuple : (heads, tails)
+    """
+
+# =============================================================================
+
+# Take Home Challenge
+
+
+def tenhead():
+    """this function counts how many times you have flipped
+    and tells you how many times the results has 10 heads, and how many tails correspondingly.
+    """
+
+
+def hot_head_streak(n):
+    """To get N streaks of head in a row.
+    How many tiems I need to flip this coin
+    Args:
+        n (int): Number of streaks
+    Returns:
+        int: How many times you need to flip this coin
+    """
+
+
+def hot_streaks(n):
+    """How many times I need to flip to get N streaks of either head of tails
+    Args:
+        n (int): Number of you streaks desired
+    Returns:
+        int: How many flips
+    """
